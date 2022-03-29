7d528ec6d200ffbe4fe578fd01bf7941909244d0
cmc <202344175@qq.com>
2022-03-29 15:42:44 +0800

    添加png解析

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const log4js = require("log4js");
 const express = require('express')
 const multer = require('multer')
 const jpeg = require('jpeg-js')
-
-// var PNG = require('png-js');
-
+const png = require('@vivaxy/png')
 
 const tf = require('@tensorflow/tfjs')
 const nsfw = require('nsfwjs')
@@ -22,13 +20,15 @@ var errLogger = log4js.getLogger('err');
 
 let _model
 
-const convert = async (img) => {
+const convert = async (img, type) => {
     // Decoded image in UInt8 Byte array
-// var myimage = new PNG(img);
-// let a = await myimage.decode()
-// console.log(a);
+    let image
+    if (type === 0) {
+        image = png.decode(img);
 
-    const image = await jpeg.decode(img, true)
+    } else if (type === 1) {
+        image = await jpeg.decode(img, true)
+    }
     const numChannels = 3
     const numPixels = image.width * image.height
     const values = new Int32Array(numPixels * numChannels)
@@ -40,28 +40,43 @@ const convert = async (img) => {
     return tf.tensor3d(values, [image.height, image.width, numChannels], 'int32')
 }
 
+/* 默认接口 */
 app.get('/', (req, res) => {
-    res.send({ msg: 'hello' })
+    res.send({ code: 200, msg: 'hello' })
 })
 
-app.post('/image', upload.single('image'), async (req, res) => {
-    // console.log(req.file.mimetype);
-    // image/png
+const fn = async (req, res) => {
+    // 作者能力有限,仅支持png与jpg
+    const imgType = ['image/png', 'image/jpeg'].indexOf(req.file.mimetype)
+    if (imgType === -1) {
+        res.send({
+            neutral: 1,//中性
+            drawing: 0,//绘画
+            sexy: 0,//性感
+            porn: 0,//色情
+            hentai: 0//变态?
+        })
+        return
+    }
     if (!req.file) res.status(400).send('Missing image multipart/form-data')
     else {
         try {
-            const image = await convert(req.file.buffer)
+            const image = await convert(req.file.buffer, imgType)
             const predictions = await _model.classify(image)
             image.dispose()
             res.json(formatOutputData(predictions))
             toWriteLogs('info', formatOutputData(predictions))
         } catch (error) {
-            res.send({ code: 500 })
+            res.send({ code: 500, msg: '服务器错误' })
             toWriteLogs('error', error)
         }
 
     }
-})
+}
+/* 检查图片 */
+app.post('/check', upload.single('image'), fn)
+app.post('/image', upload.single('image'), fn)
+
 
 const formatOutputData = (predictions, outVal = {}) => {
     predictions.forEach(item => {
@@ -78,11 +93,9 @@ const toWriteLogs = (type, data) => {
     }
 }
 
-
 const load_model = async () => {
     _model = await nsfw.load()
 }
-
 
 load_model().then(() => {
     app.listen(port, () => {
@@ -92,6 +105,3 @@ load_model().then(() => {
     .catch(err => {
         toWriteLogs('error', err)
     })
-
-
-// curl --request POST localhost:8080/nsfw --header 'Content-Type: multipart/form-data' --data-binary 'image=@/full/path/to/picture.jpg'
