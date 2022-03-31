5e87d919bb931edf3dd51523efc3628abe6bb8dc
mingchen3398 <1976326+mingchen3398@user.noreply.gitee.com>
2022-03-31 09:35:27 +0000

    update config/log4js.js. 保留日志限制为48个

--- config/log4js.js
+++ config/log4js.js
@@ -3,17 +3,17 @@ module.exports =  {
         infoLogs: {
             type: 'dateFile',
             filename: 'logs/info/info.log',
-            backups: 200,  // 仅保留最新的五个日志文件
+            backups: 48, 
             pattern: ".yyyy-MM-dd-hh",
             alwaysIncludePattern: true,
-            compress: true
+            //compress: true
         },
         errorLogs: {
             type: 'dateFile',
             filename: 'logs/error/error.log',
             pattern: ".yyyy-MM-dd-hh",
-            backups: 200,
-            compress: true
+            backups: 48,
+            //compress: true
         }
     },
     categories: {
